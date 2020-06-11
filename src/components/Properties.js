9a0cf98638be302b1777388a9c746755d38ed740
Jacob Kenyon <jacob.kenyon@distrelec.com>
2020-06-11 11:58:12 +0100

    changing class components to functional components with hooks

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,42 +1,37 @@
 import React from 'react';
 import { Item, Label, Container, Loader, Dimmer } from 'semantic-ui-react';
 
-class Properties extends React.Component {
+function Properties({feed}) {
+    const isLoading = (feed.length === 0 ? false : true);
+    const renderList = feed.map((i) =>
+        <Item key={i.id} className="property__items__item">
+            <Item.Image size='large' src={i.propertyImages} />
+            <Item.Content>
+                <Item.Header target="_blank" as='a' href={'https://rightmove.co.uk' + i.propertyUrl}>{i.propertyTypeFullDescription}</Item.Header>
+                <Item.Description>
+                    <p className="title">{i.displayAddress}</p>
+                    <p>{i.summary}</p>
+                </Item.Description>
+                <Item.Extra className="property__additional">
+                    <Label className="property__additional--price"> £{i.price}</Label>
+                    <Label className="property__additional--desc" icon='globe' content={i.formattedBranchName} />
+                </Item.Extra>
+            </Item.Content>
+        </Item>
+    );
 
-    render() {
-        const feed = this.props.feed;
-        const isLoading = (this.props.feed.length === 0 ? false : true);
-        const renderList = feed.map((i) =>
-                <Item key={i.id} className="property__items__item">
-                    <Item.Image size='large' src={i.propertyImages} />
-                    <Item.Content>
-                        <Item.Header target="_blank" as='a' href={'https://rightmove.co.uk' + i.propertyUrl}>{i.propertyTypeFullDescription}</Item.Header>
-                        <Item.Description>
-                            <p className="title">{i.displayAddress}</p>
-                            <p>{i.summary}</p>
-                        </Item.Description>
-                        <Item.Extra className="property__additional">
-                            <Label className="property__additional--price"> £{i.price}</Label>
-                            <Label className="property__additional--desc" icon='globe' content={i.formattedBranchName} />
-                        </Item.Extra>
-                    </Item.Content>
-                </Item>
-        );
-
-      return(
-          <Container className="property">
-              {!isLoading &&
-                <Dimmer active>
-                    <Loader />
-                </Dimmer>
-              }
-              <Item.Group className="property__items">
-                  {renderList}
-              </Item.Group>
-          </Container>
-      )
-
-    }
+ return (
+     <Container className="property">
+         {!isLoading &&
+         <Dimmer active>
+             <Loader />
+         </Dimmer>
+         }
+         <Item.Group className="property__items">
+             {renderList}
+         </Item.Group>
+     </Container>
+ )
 
 }
 
--- src/components/Scraper.js
+++ src/components/Scraper.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Properties from './Properties';
 
-class Scraper extends React.Component{
+function Scraper() {
 
-    state = { data: [] };
+    const [data, setData] = useState([]);
 
-    componentDidMount() {
-        this.scrapePage();
-    }
+    useEffect(() => {
+        scrapePage();
+    }, [])
 
-    scrapePage = () => {
+    const scrapePage = () => {
         const self = this;
         axios.get('https://cors-anywhere.herokuapp.com/https://www.rightmove.co.uk/api/_search?locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22%7BwafI%60vzNisBlhCciAwxBx%7BAoxFaKyp%40kf%40uiDjZeeDxe%40A%7Cj%40se%40t%60%40rcAhNnTjSrV%60YzZSrVT%7Ci%40py%40t%7CCq%60AuXub%40~iArcBql%40%60jBtpI%7B%7CDtvAu%5EyfC%22%7D&minBedrooms=2&maxPrice=180000&minPrice=100000&numberOfPropertiesPerPage=72&radius=0.0&sortType=0&index=0&propertyTypes=terraced%2Csemi-detached&viewType=LIST&mustHave=garden&channel=BUY&areaSizeUnit=sqft&currencyCode=GBP&isFetching=false')
             .then(response => {
                 const properties = response.data.properties;
                 properties.forEach((i) => {
-                    const {summary, displayAddress, price, propertyImages, propertyTypeFullDescription, formattedBranchName, id, propertyUrl} = i;
-                    self.setState(prevState => ({
-                        data: [{
+                        const {summary, displayAddress, price, propertyImages, propertyTypeFullDescription, formattedBranchName, id, propertyUrl} = i;
+                        setData( (prevState) => [...prevState, {
                             summary,
                             displayAddress,
                             price: price.amount,
@@ -27,21 +26,18 @@ class Scraper extends React.Component{
                             propertyImages: propertyImages.mainImageSrc,
                             id,
                             propertyUrl
-                        }, ...prevState.data]
-                    }))
+                        }]);
                 });
 
             })
+
     }
 
-    render() {
-        return(
-            <div>
-
-               <Properties feed={this.state.data} />
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Properties feed={data} />
+        </div>
+    )
 
 }
 
